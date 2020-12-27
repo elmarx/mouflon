119af89a1dfc653043e58787d116600a1cfc143e
Elmar Athmer <elmar@athmer.org>
2020-12-27 01:40:34 +0100

    implement access_code flow

--- src/token/oauth2.rs
+++ src/token/oauth2.rs
@@ -1,14 +1,20 @@
+use crate::browser::open_url_in_browser;
 use crate::config::{ClientConfig, Endpoint};
 use crate::model::AccessTokenResponse;
 use crate::token::openid_configuration::OpenIdConfiguration;
+use crate::token::pkce::{url_safe_rand, verifier_to_challenge};
+use crate::token::receive_code::receive_code;
 use crate::BoxResult;
 use bytes::buf::BufExt;
 use hyper::body::aggregate;
 use hyper::client::HttpConnector;
 use hyper::header::CONTENT_TYPE;
-use hyper::{Body, Client, Request, Response};
+use hyper::{Body, Client, Request};
 use hyper_tls::HttpsConnector;
+use log::error;
 use mime::APPLICATION_WWW_FORM_URLENCODED;
+use serde::de::DeserializeOwned;
+use serde_json::Value;
 
 pub struct Oauth2Client {
     client: Client<HttpsConnector<HttpConnector>>,
@@ -45,7 +51,13 @@ impl Oauth2Client {
         })
     }
 
-    async fn token_request(&self, body: String) -> BoxResult<Response<Body>> {
+    fn client_secret_str(&self) -> &str {
+        self.client_secret
+            .as_ref()
+            .map_or_else(|| "", |s| s.as_str())
+    }
+
+    async fn token_request<T: DeserializeOwned>(&self, body: String) -> BoxResult<T> {
         let body = Body::from(body);
 
         let req = Request::builder()
@@ -54,26 +66,62 @@ impl Oauth2Client {
             .header(CONTENT_TYPE, APPLICATION_WWW_FORM_URLENCODED.as_ref())
             .body(body)?;
 
-        Ok(self.client.request(req).await?)
+        let response = self.client.request(req).await?;
+        let body = aggregate(response).await?;
+
+        let json: Value = serde_json::from_reader(body.reader())?;
+
+        match T::deserialize(&json) {
+            Ok(v) => Ok(v),
+            Err(err) => {
+                error!("error reading response {}: {:?}", json, err);
+                Err(Box::new(err))
+            }
+        }
     }
 
     pub async fn refresh_token(&self, rt: &str) -> BoxResult<AccessTokenResponse> {
         let req_body = format!(
             "grant_type=refresh_token&client_id={}&client_secret={}&refresh_token={}",
             self.client_id,
-            self.client_secret
-                .as_ref()
-                .map_or_else(|| "", |s| s.as_str()),
+            self.client_secret_str(),
             rt
         );
 
-        let response = self.token_request(req_body).await?;
-        let body = aggregate(response).await?;
-
-        Ok(serde_json::from_reader(body.reader())?)
+        self.token_request(req_body).await
     }
 
     pub async fn access_token(&self) -> BoxResult<AccessTokenResponse> {
-        todo!("execute access_token flow")
+        let stored_state = url_safe_rand(16);
+        let code_verifier = url_safe_rand(64);
+        let code_challenge = verifier_to_challenge(&*code_verifier);
+        let redirect_uri = "http://localhost:4800/";
+
+        let auth_url = format!(
+            "{}?client_id={}&redirect_uri={}&response_type=code&code_challenge={}&code_challenge_method=S256&state={}",
+            self.authorization_endpoint,
+            self.client_id,
+            redirect_uri,
+            code_challenge,
+            stored_state
+        );
+
+        open_url_in_browser(&*auth_url)?;
+
+        let returned = receive_code().await?;
+        if returned.state != stored_state {
+            todo!("handle CSRF issue")
+        }
+
+        let auth_code_params = format!(
+            "grant_type=authorization_code&client_id={}&client_secret={}&redirect_uri={}&code={}&code_verifier={}",
+            self.client_id,
+            self.client_secret_str(),
+            redirect_uri,
+            returned.code,
+            code_verifier
+        );
+
+        Ok(self.token_request(auth_code_params).await?)
     }
 }
--- src/token/receive_code.rs
+++ src/token/receive_code.rs
@@ -1,3 +1,4 @@
+use crate::BoxResult;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Response, Server};
 use std::collections::HashMap;
@@ -5,9 +6,14 @@ use std::convert::Infallible;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
+pub(crate) struct ReturnParams {
+    pub code: String,
+    pub state: String,
+}
+
 /// start a webserver and return the query-parameter "code"
-pub async fn receive_code() -> String {
-    let (tx, rx) = tokio::sync::oneshot::channel::<String>();
+pub(crate) async fn receive_code() -> BoxResult<ReturnParams> {
+    let (tx, rx) = tokio::sync::oneshot::channel::<ReturnParams>();
     let tx = Arc::new(Mutex::new(Some(tx)));
 
     let make_svc = make_service_fn(move |_conn| {
@@ -30,7 +36,9 @@ pub async fn receive_code() -> String {
                     let tx = tx.clone();
 
                     if let Some(tx) = tx.lock().await.take() {
-                        let _ = tx.send(params.get("code").unwrap().to_owned());
+                        let code = params.get("code").unwrap().to_owned();
+                        let state = params.get("state").unwrap().to_owned();
+                        let _ = tx.send(ReturnParams { code, state });
                     }
 
                     Ok::<_, Infallible>(Response::new(Body::from("I may have received the code.")))
@@ -39,16 +47,16 @@ pub async fn receive_code() -> String {
         }
     });
 
-    let addr = ([127, 0, 0, 1], 3000).into();
+    let addr = ([127, 0, 0, 1], 4800).into();
 
     let server = Server::bind(&addr).serve(make_svc);
 
-    let mut code: Option<String> = None;
+    let mut return_params: Option<ReturnParams> = None;
     let graceful = server.with_graceful_shutdown(async {
-        code = rx.await.ok();
+        return_params = rx.await.ok();
     });
 
-    graceful.await.unwrap();
+    graceful.await?;
 
-    code.unwrap()
+    Ok(return_params.unwrap())
 }
