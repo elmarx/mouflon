d8412c7d4361545c947104b6b8b22181b5c5c909
Elmar Athmer <elmar.athmer@rewe-digital.com>
2021-11-11 10:38:41 +0100

    upgrade dependencies (fix for deno 1.16)

--- mouflon.ts
+++ mouflon.ts
@@ -1,17 +1,17 @@
 #!/usr/bin/env -S deno run --unstable --allow-net --allow-read --allow-env --allow-write --allow-run
 
-import { Application, Router } from "https://deno.land/x/oak@v6.3.2/mod.ts";
-import addSeconds from "https://deno.land/x/date_fns@v2.15.0/addSeconds/index.js";
-import parseIso from "https://deno.land/x/date_fns@v2.15.0/parseISO/index.js";
+import { Application, Router } from "https://deno.land/x/oak@v9.0.1/mod.ts";
+import addSeconds from "https://deno.land/x/date_fns@v2.22.1/addSeconds/index.ts";
+import parseIso from "https://deno.land/x/date_fns@v2.22.1/parseISO/index.js";
 
 import { createHash } from "https://deno.land/std@0.82.0/hash/mod.ts";
-import { ensureDir, exists } from "https://deno.land/std@0.82.0/fs/mod.ts";
-import { join } from "https://deno.land/std@0.82.0/path/mod.ts";
-import { assert } from "https://deno.land/std@0.82.0/testing/asserts.ts";
-import { deferred } from "https://deno.land/std@0.82.0/async/deferred.ts";
-import { parse } from "https://deno.land/std@0.82.0/flags/mod.ts";
+import { ensureDir, exists } from "https://deno.land/std@0.114.0/fs/mod.ts";
+import { join } from "https://deno.land/std@0.114.0/path/mod.ts";
+import { assert } from "https://deno.land/std@0.114.0/testing/asserts.ts";
+import { deferred } from "https://deno.land/std@0.114.0/async/deferred.ts";
+import { parse } from "https://deno.land/std@0.114.0/flags/mod.ts";
 import { passwordGenerator } from "https://deno.land/x/password_generator/mod.ts";
-import { encode as encodeBase64Url } from "https://deno.land/std@0.82.0/encoding/base64url.ts";
+import { encode as encodeBase64Url } from "https://deno.land/std@0.114.0/encoding/base64url.ts";
 
 const MOUFLON_PORT = parseInt(Deno.env.get("MOUFLON_PORT") || "4800");
 
